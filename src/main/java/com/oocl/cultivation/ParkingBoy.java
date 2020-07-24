97531107b5b125104a3ccc429ff4115911d9e0d7
hardy <hard.t.z.wei@oocl.com>
2020-07-24 22:25:58 +0800

    [story2][hardy]feature :add implement about AC1

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation;
 public class ParkingBoy {
 
     private ParkingLot parkingLot;
+    private String errorMsg;
 
     public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
@@ -14,11 +15,14 @@ public class ParkingBoy {
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
-
+        if(parkingTicket == null || parkingTicket.getUsed()) {
+            this.errorMsg = "Unrecognized parking ticket.";
+            return null;
+        }
         return this.parkingLot.fetch(parkingTicket);
     }
 
-    public String query() {
-        return "";
+    public String query(){
+        return this.errorMsg;
     }
 }
